97b93ba16b4e375624258dcebfc94ed3f8a56677
Steve <stevedsimkins@gmail.com>
2025-01-29 10:14:19 -0500

    chore: Test RPC

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,10 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+app.post('/id', async (c) => {
+  const req = await fetch('https://kubo.railway.internal:5001/api/v0/id')
+  const res = await req.json()
+  return c.json(res, 200)
+})
+
 export default app
